5280c4487ee4106663c9a4692a0c2b5f78fe2d1c
JMKim <tdcian71@gmail.com>
2025-03-22 13:27:48 +0900

    made vision os menu bar

---
+++ VisionOSMenuBarSwiftUI/VisionOSMenuBarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSMenuBarSwiftUIApp.swift
+//  VisionOSMenuBarSwiftUI
+//
+//  Created by 김정민 on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSMenuBarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
